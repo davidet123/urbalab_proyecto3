83d5163ef269450542edb6c4780b8dd4a36b45bb
David Delgado <davidet123@gmail.com>
2022-09-20 19:42:20 +0200

    Funcionalidad para mostrat/esconder páginas

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
 const boton = document.getElementById("boton1")
+const botonEnviar = document.getElementById("enviarForm")
 
 let inicio = document.getElementsByClassName("web-inicio")
 let formulario = document.getElementsByClassName("login-page")
+let aplicacion = document.getElementsByClassName("aplicacion")
 
 boton.addEventListener("click", e => {
   e.preventDefault();
   console.log(inicio)
   inicio[0].style.display = "none"
   formulario[0].style.display = "flex"
+
+})
+
+botonEnviar.addEventListener("click", e => {
+  e.preventDefault();
+  formulario[0].style.display = "none"
+  aplicacion[0].style.display = "flex"
 
 })
