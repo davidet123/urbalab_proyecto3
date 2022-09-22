6844416590b19a9669c6fda33e50c4a55ae32f35
David Delgado <davidet123@gmail.com>
2022-09-22 20:51:17 +0200

    Añadida tarjeta información al pasar el ratón sobre el item

--- js/main.js
+++ js/main.js
@@ -27,4 +27,44 @@ botonEnviar.addEventListener("click", (e) => {
 });
 
 
-console.log("test")
+
+
+// Event listener para items compra
+ let items = document.querySelectorAll(".item-compra")
+
+ let info = document.getElementById("info");
+ let textoInfo = document.querySelector("#info p")
+
+
+ const offsetX = 20;
+ const offsetY = 10;
+
+ items.forEach(item => {
+  item.addEventListener("mouseover", (e) => {
+    console.log(e)
+    info.style.display="block"
+    info.style.left = `${e.x + offsetX}px`
+    info.style.top = `${e.y+ offsetY}px`
+    textoInfo.innerHTML = `${e.x + offsetX}px`
+    /* info.style.left = e.clientX  */
+  })
+ });
+
+ items.forEach(item => {
+  item.addEventListener("mousemove", (e) => {
+    info.style.left = `${e.x  + offsetX}px`
+    info.style.top = `${e.y  + offsetY}px`
+  })
+ });
+
+ items.forEach(item => {
+  item.addEventListener("mouseleave", (e) => {
+    console.log(e)
+    info.style.display="none"
+  })
+ });
+
+ // Mostrar ventana info al pasar el cursor por encima del elemento
+
+
+
